bad7e51595c4521ffa8e9d4ccf2c733bb1ee67ed
Marek Arendt <marek.arendt@yougov.com>
2022-07-08 14:13:27 +0200

    new model created

--- home/models.py
+++ home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
 
 from wagtail.models import Page
 
 
 class HomePage(Page):
     pass
+
+
+class NewModel(Page):
+    second_title = models.CharField(max_length=255, null=True, blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    footer = models.CharField(max_length=255, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("second_title"),
+        FieldPanel("description"),
+        FieldPanel("footer"),
+    ]
