0effd916f90b2e15d47f0a4339cfe153c904e393
Marek Arendt <marek.arendt@yougov.com>
2022-07-08 13:30:12 +0200

    new wagtail project initiated

---
+++ test_migrations/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wt#x(q11--4_p#dmb^(fwb!iik@cc+&2!h(^2vfrb%xe%b6vbu"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
